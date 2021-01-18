16cb89048d221ace422ebfd124684b7a9044ce28
JiaChen-Lee <1286779567@qq.com>
2021-01-18 16:52:13 +0800

    DATA.md

--- my_test.py
+++
@@ -1,9 +0,0 @@
-# Created by Jiachen Li at 2021/1/4 22:39
-import torch
-
-x = torch.tensor([1, 2, 3, 4])
-print(x.shape)
-y1 = torch.unsqueeze(x, 0)
-print(y1.shape)
-y2 = torch.unsqueeze(x, 1)
-print(y2.shape)
